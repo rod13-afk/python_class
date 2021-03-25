d9b69edcf47526cbc7e25cd0ad9915db2ca264a1
Rodrigo Daniel Hernandez Barrera <80066340+rod13-afk@users.noreply.github.com>
2021-03-25 09:56:19 -0600

    Se creo de la carpeta Templates y el archivo template.py
    El archivo template.py contiene el encabezado del programa AT-percentage. Este formato con el paso del curso se irá actualizando.

---
+++ Templates/template.py
@@ -0,0 +1,34 @@
+'''
+NAME
+       AT-percentage
+
+VERSION
+        1.0
+
+AUTHOR
+        Rodrigo Daniel Hernández Barrera        
+
+DESCRIPTION
+        Calculate percentage of AT on a genome sequence
+
+CATEGORY
+        Genomic sequence
+
+USAGE
+
+    % python AT-percentage.py -i filename
+    
+    example
+    
+    % python AT-percentage.py -i sequence.txt
+        
+'''
+    
+
+## 1.
+
+## 2.
+
+## 3.
+
+## 4.
